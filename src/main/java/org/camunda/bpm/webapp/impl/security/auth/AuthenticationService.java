c83f231d00c74a19de1495dc5ca0f8e17699f2d0
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2019-01-21 15:16:13 +0100

    chore(webapps): tolerate invalid groups and tenants
    related to CAM-9651

--- src/main/java/org/camunda/bpm/webapp/impl/security/auth/AuthenticationService.java
+++ src/main/java/org/camunda/bpm/webapp/impl/security/auth/AuthenticationService.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -98,7 +98,10 @@ public class AuthenticationService {
 
     List<String> tenantIds = new ArrayList<String>();
     for(Tenant tenant : tenants) {
-      tenantIds.add(tenant.getId());
+      if (tenant != null && tenant.getId() != null)
+      {
+        tenantIds.add(tenant.getId());
+      }
     }
     return tenantIds;
   }
@@ -110,7 +113,10 @@ public class AuthenticationService {
 
     List<String> groupIds = new ArrayList<String>();
     for (Group group : groups) {
-      groupIds.add(group.getId());
+      if (group != null && group.getId() != null)
+      {
+        groupIds.add(group.getId());
+      }
     }
     return groupIds;
   }
