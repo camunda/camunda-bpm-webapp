9e78c985e02a5ca3ac4f53a29ca9019158b2be0f
Martin Stamm <martin.stamm@camunda.com>
2019-08-22 14:18:09 +0200

    fix(admin): disable user sorting when using LDAP
    related to CAM-10740

--- ui/admin/client/scripts/pages/users.js
+++ ui/admin/client/scripts/pages/users.js
@@ -20,100 +20,152 @@
 var fs = require('fs');
 
 var template = fs.readFileSync(__dirname + '/users.html', 'utf8');
-var searchConfig = JSON.parse(fs.readFileSync(__dirname + '/users-search-plugin-config.json', 'utf8'));
+var searchConfig = JSON.parse(
+  fs.readFileSync(__dirname + '/users-search-plugin-config.json', 'utf8')
+);
 
 var angular = require('camunda-commons-ui/vendor/angular');
 
-var Controller = ['$scope', '$location', 'search', 'UserResource', 'page', '$translate', function($scope, $location, search, UserResource, pageService, $translate) {
-  $scope.searchConfig = angular.copy(searchConfig);
+var Controller = [
+  '$scope',
+  '$location',
+  'search',
+  'UserResource',
+  'page',
+  '$translate',
+  'Notifications',
+  function(
+    $scope,
+    $location,
+    search,
+    UserResource,
+    pageService,
+    $translate,
+    Notifications
+  ) {
+    $scope.searchConfig = angular.copy(searchConfig);
 
-  angular.forEach(searchConfig.tooltips, function(translation, tooltip) {
-    $scope.searchConfig.tooltips[tooltip] = $translate.instant(translation);
-  });
+    angular.forEach(searchConfig.tooltips, function(translation, tooltip) {
+      $scope.searchConfig.tooltips[tooltip] = $translate.instant(translation);
+    });
 
-  $scope.searchConfig.types.map(function(type) {
-    type.id.value = $translate.instant(type.id.value);
-    if (type.operators) {
-      type.operators = type.operators.map(function(op) {
-        op.value = $translate.instant(op.value);
-        return op;
-      });
-    }
-    return type;
-  });
+    $scope.searchConfig.types.map(function(type) {
+      type.id.value = $translate.instant(type.id.value);
+      if (type.operators) {
+        type.operators = type.operators.map(function(op) {
+          op.value = $translate.instant(op.value);
+          return op;
+        });
+      }
+      return type;
+    });
 
-  $scope.blocked = true;
-  $scope.onSearchChange = updateView;
+    $scope.blocked = true;
+    $scope.onSearchChange = updateView;
 
-  $scope.query = $scope.pages = null;
-  var sorting;
+    $scope.canSortEntries = true;
 
-  $scope.onSortInitialized = function(_sorting) {
-    sorting = _sorting;
-    $scope.blocked = false;
-  };
+    $scope.query = $scope.pages = null;
+    var sorting;
 
-  $scope.onSortChanged = function(_sorting) {
-    sorting = _sorting;
-    updateView();
-  };
-
-  function updateView(query, pages) {
-    if (query && pages) {
-      $scope.query = query;
-      $scope.pages = pages;
-    }
-
-    var page = $scope.pages.current,
-        count = $scope.pages.size,
-        firstResult = (page - 1) * count;
-
-    var queryParams = {
-      firstResult: firstResult,
-      maxResults: count,
-      sortBy: sorting.sortBy,
-      sortOrder: sorting.sortOrder
+    $scope.onSortInitialized = function(_sorting) {
+      sorting = _sorting;
+      $scope.blocked = false;
     };
 
-    $scope.userList = null;
-    $scope.loadingState = 'LOADING';
+    $scope.onSortChanged = function(_sorting) {
+      sorting = _sorting;
+      updateView();
+    };
 
-    return UserResource.count(angular.extend({}, $scope.query)).$promise.then(function(data) {
-      var total = data.count;
+    function updateView(query, pages) {
+      if (query && pages) {
+        $scope.query = query;
+        $scope.pages = pages;
+      }
 
-      return UserResource.query(angular.extend({}, $scope.query, queryParams)).$promise.then(function(data) {
-        $scope.userList = data;
-        $scope.loadingState = data.length ? 'LOADED' : 'EMPTY';
+      var page = $scope.pages.current,
+          count = $scope.pages.size,
+          firstResult = (page - 1) * count;
 
-        return total;
-      }).catch(angular.noop);
-    }).catch(angular.noop);
-  }
+      var queryParams = {
+        firstResult: firstResult,
+        maxResults: count,
+        sortBy: sorting.sortBy,
+        sortOrder: sorting.sortOrder
+      };
 
-  $scope.availableOperations = {};
-  UserResource.OPTIONS().$promise.then(function(response) {
-    angular.forEach(response.links, function(link) {
-      $scope.availableOperations[link.rel] = true;
+      $scope.userList = null;
+      $scope.loadingState = 'LOADING';
+
+      return UserResource.count(angular.extend({}, $scope.query))
+        .$promise.then(function(data) {
+          var total = data.count;
+
+          return UserResource.query(
+            angular.extend({}, $scope.query, queryParams)
+          )
+            .$promise.then(function(data) {
+              $scope.userList = data;
+              $scope.loadingState = data.length ? 'LOADED' : 'EMPTY';
+
+              return total;
+            })
+            .catch(function() {
+              // When using LDAP, sorting parameters might not work and throw errors
+              // Try again with default sorting
+              delete queryParams.sortBy;
+              delete queryParams.sortOrder;
+              return UserResource.query(
+                angular.extend({}, $scope.query, queryParams)
+              ).$promise.then(function(data) {
+                $scope.canSortEntries = false;
+                $scope.userList = data;
+                $scope.loadingState = data.length ? 'LOADED' : 'EMPTY';
+
+                Notifications.addMessage({
+                  status: $translate.instant('USERS_NO_SORTING_HEADER'),
+                  message: $translate.instant('USERS_NO_SORTING_BODY'),
+                  exclusive: true
+                });
+
+                return total;
+              });
+            });
+        })
+        .catch(angular.noop);
+    }
+
+    $scope.availableOperations = {};
+    UserResource.OPTIONS()
+      .$promise.then(function(response) {
+        angular.forEach(response.links, function(link) {
+          $scope.availableOperations[link.rel] = true;
+        });
+      })
+      .catch(angular.noop);
+
+    $scope.$root.showBreadcrumbs = true;
+
+    pageService.titleSet($translate.instant('USERS_USERS'));
+
+    pageService.breadcrumbsClear();
+
+    pageService.breadcrumbsAdd({
+      label: $translate.instant('USERS_USERS'),
+      href: '#/users/'
     });
-  }).catch(angular.noop);
+  }
+];
 
-  $scope.$root.showBreadcrumbs = true;
-
-  pageService.titleSet($translate.instant('USERS_USERS'));
-
-  pageService.breadcrumbsClear();
-
-  pageService.breadcrumbsAdd({
-    label: $translate.instant('USERS_USERS'),
-    href: '#/users/'
-  });
-}];
-
-module.exports = [ '$routeProvider', function($routeProvider) {
-  $routeProvider.when('/users', {
-    template: template,
-    controller: Controller,
-    authentication: 'required',
-    reloadOnSearch: false
-  });
-}];
+module.exports = [
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider.when('/users', {
+      template: template,
+      controller: Controller,
+      authentication: 'required',
+      reloadOnSearch: false
+    });
+  }
+];
