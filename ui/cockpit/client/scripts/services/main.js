cd59c875669b271d2dd6d9fe9d15c85e3e84150b
Tassilo Weidner <tassilo.weidner@camunda.com>
2019-10-08 10:44:50 +0200

    chore(cockpit): add input field to definition views to select version
    related to CAM-10867

--- ui/cockpit/client/scripts/services/main.js
+++ ui/cockpit/client/scripts/services/main.js
@@ -22,6 +22,7 @@ var angular = require('camunda-commons-ui/vendor/angular'),
     transform = require('./transform'),
     variables = require('./variables'),
     breadcrumbTrails = require('./breadcrumbTrails'),
+    queryMaxResults = require('./query-max-results'),
     routeUtil = require('./../../../../common/scripts/services/routeUtil'),
     page = require('./../../../../common/scripts/services/page'),
     camAPI = require('./../../../../common/scripts/services/cam-api'),
@@ -36,6 +37,7 @@ servicesModule.factory('Transform', transform);
 servicesModule.factory('Variables', variables);
 servicesModule.service('page', page);
 servicesModule.factory('breadcrumbTrails', breadcrumbTrails);
+servicesModule.factory('queryMaxResults', queryMaxResults);
 servicesModule.factory('routeUtil', routeUtil);
 servicesModule.factory('camAPI', camAPI);
 servicesModule.factory('hasPlugin', hasPlugin);
