65d8ae5f6de6400213098715fdbadd019d523ad0
Martin Stamm <martin.stamm@camunda.com>
2019-11-13 16:25:26 +0100

    Revert(webapps): issue a CSRF token directly after the login request
    This reverts commit 15e37dfa3c8989c3d2a372c8788d537a44919f9b.
    
    related to CAM-11056, CAM-10943

--- ui/tasklist/grunt/config/copy.js
+++ ui/tasklist/grunt/config/copy.js
@@ -73,17 +73,19 @@ module.exports = function(config, copyConf) {
   }
 
   copyConf.tasklist_index = {
-    options: {
-      process: copyReplace
-    },
-    files: [
-      {
-        expand: true,
-        cwd: '<%= pkg.gruntConfig.tasklistSourceDir %>',
-        src: ['index.html', 'csrf-token.html'],
-        dest: '<%= pkg.gruntConfig.tasklistBuildTarget %>/'
-      }
-    ]
+      options: {
+        process: copyReplace
+      },
+      files: [
+        {
+          expand: true,
+          cwd: '<%= pkg.gruntConfig.tasklistSourceDir %>',
+          src: [
+            'index.html'
+          ],
+          dest: '<%= pkg.gruntConfig.tasklistBuildTarget %>/',
+        }
+      ]
   };
 
   copyConf.tasklist_bootstrap = {
